59682dec9772bff04235c1ab299daee8570b91f1
wlals2997 <85855544+wlals2997@users.noreply.github.com>
2021-11-12 15:30:41 +0900

    JS Home main imageslide

---
+++ main.js
@@ -0,0 +1,13 @@
+const SHOWING_CLASS="showing";
+
+console.log(mainImage)
+
+function onSlide() {
+    const mainImage = document.querySelector('.home__image-left:first-child');
+    const lastImage = document.querySelector('.home__image-left:last-child');
+    if() {
+       mainImage.classList.add('hidden') 
+    }
+}
+onSlide()
+setInterval(onSlide, 3000);
