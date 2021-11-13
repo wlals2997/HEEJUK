94a1460efda5f5bbbac3c43728fa50a1bbe4295e
wlals2997 <85855544+wlals2997@users.noreply.github.com>
2021-11-13 23:59:37 +0900

    JS slide function

--- main.js
+++ main.js
@@ -2,24 +2,24 @@ const SHOWING_CLASS = 'showing';
 const firstSlide = document.querySelector('.home__image-left');
 
 function onSlide() {
- 
+   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`); //showing class이다.
+
+if (currentSlide) {
+  currentSlide.classList.remove(SHOWING_CLASS);
+  const nextSlide = currentSlide.nextElementSibling;
+
+  if (nextSlide) {
+    nextSlide.classList.add(SHOWING_CLASS);
+  } else {
+    firstSlide.classList.add(SHOWING_CLASS);
+  }
+
+} else {
+  firstSlide.classList.add(SHOWING_CLASS);
+}
 }
 
 onSlide();
 setInterval(onSlide, 3000);
 
-//   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`); //showing class이다.
 
-// if (currentSlide) {
-//   currentSlide.classList.remove(SHOWING_CLASS);
-//   const nextSlide = currentSlide.nextElementSibling;
-
-//   if (nextSlide) {
-//     nextSlide.classList.add(SHOWING_CLASS);
-//   } else {
-//     firstSlide.classList.add(SHOWING_CLASS);
-//   }
-
-// } else {
-//   firstSlide.classList.add(SHOWING_CLASS);
-// }
